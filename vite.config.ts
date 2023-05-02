704895da9b9c854ecfe59313d057dcf1828b8d48
Hayao S <main@den4dr.com>
2023-05-02 23:54:56 +0900

    feature: add settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import linaria from '@linaria/vite';
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react-swc";
+import linaria from "@linaria/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), linaria()],
-})
+});
